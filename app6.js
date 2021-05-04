95935b12e34ac156d305a1239f61597acb212a0f
jadgal5190 <83171173+jadgal5190@users.noreply.github.com>
2021-05-04 19:23:23 +0430

    Update app6.js

--- app6.js
+++ app6.js
@@ -1,6 +1,6 @@
 var milad = document.createElement('div');
  milad.className = "server-n";
- milad.style.cssText = "position: relative;-webkit-animation: colorchange 2s infinite alternate;margin-left: 4.5vw;width: 10.5vw;transition: all 0.3s ease 0s;height: 1.7vw;font-size: 1.1vw;color: white;text-align: center;letter-spacing: 0.5vw;border-left: 0.2vw solid RGBA(255,101,80,1);padding-left: 0.5vw;border-right: 0.2vw solid RGBA(255,101,80,1);border-radius: 0.5vw;background-color: RGBA(255,101,80,0.5);display: block !important;"
+ milad.style.cssText = "position: relative;-webkit-animation: colorchange 2s infinite alternate;margin-left: 4.5vw;width: 10.5vw;transition: all 0.3s ease 0s;height: 1.7vw;font-size: 1.1vw;color: white;text-align: center;letter-spacing: 0.5vw;border-left: 0.4vw solid RGBA(255,101,80,1);padding-left: 0.5vw;border-right: 0.4vw solid RGBA(255,101,80,1);border-radius: 0.5vw;background-color: RGBA(255,101,80,0.5);display: block !important;"
  milad.innerHTML = "<span>OverLand</span>";
  document.getElementById('player-header').appendChild(milad);
  
