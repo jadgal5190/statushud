e114b713be6d397c3b7cd625e9a49abdab155834
jadgal5190 <83171173+jadgal5190@users.noreply.github.com>
2021-05-04 19:20:46 +0430

    Update app4.js

--- app4.js
+++ app4.js
@@ -1,11 +1,11 @@
 var milad = document.createElement('div');
  milad.className = "server-n";
- milad.style.cssText = "position: relative;-webkit-animation: colorchange 2s infinite alternate;margin-left: 4.5vw;width: 10.5vw;transition: all 0.3s ease 0s;height: 1.7vw;font-size: 1.1vw;color: white;text-align: center;letter-spacing: 0.5vw;border-left: 0.2vw solid RGBA(96,125,139,0.7);padding-left: 0.5vw;border-right: 0.2vw solid RGBA(96,125,139,0.7);border-radius: 0.5vw;background-color: RGBA(96,125,139,0.5);display: block !important;"
+ milad.style.cssText = "position: relative;-webkit-animation: colorchange 2s infinite alternate;margin-left: 4.5vw;width: 10.5vw;transition: all 0.3s ease 0s;height: 1.7vw;font-size: 1.1vw;color: white;text-align: center;letter-spacing: 0.5vw;border-left: 0.2vw solid RGBA(96,125,139,0.7);padding-left: 0.5vw;border-right: 0.2vw solid RGBA(96,125,139,0.7);border-radius: 0.5vw;background-color: RGBA(255,101,80,0.5);display: block !important;"
  milad.innerHTML = "<span>OverLand</span>";
  document.getElementById('player-header').appendChild(milad);
  
  var milad = document.createElement('div');
  milad.className = "designe";
  milad.innerHTML = "<small>Designed By MiLaD#0125</small>";
- milad.style.cssText = "display:none;top: -1.0vw;letter-spacing: 0.05vw;position: absolute;padding: 0 0.2vw;transition:0.3s;background: rgba(96, 125, 139, 0.5);font-size: 0.6vw;border-radius: 0.2vw 0.2vw 0.2vw 0.2vw;color: #fff;right: 2vw;"
+ milad.style.cssText = "display:none;top: -1.0vw;letter-spacing: 0.05vw;position: absolute;padding: 0 0.2vw;transition:0.3s;background: RGBA(255,101,80,0.5);font-size: 0.6vw;border-radius: 0.2vw 0.2vw 0.2vw 0.2vw;color: #fff;right: 2vw;"
  document.getElementById('player-header').appendChild(milad);
