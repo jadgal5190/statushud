7d8cff2074ad2bb7981cef48e29051c9dff6c5ac
jadgal5190 <83171173+jadgal5190@users.noreply.github.com>
2021-05-04 17:57:25 +0430

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var milad = document.createElement('div');
- milad.className = "server-name";
+ milad.className = "server-n";
  milad.style.cssText = "display:block !important;position: relative;margin-left: 5.0vw;width: 13.0vw;transition:all ease 0.3s;height: 1.7vw;font-size: 1.1vw;color: white;text-align: center;letter-spacing: 0.5vw;padding-left:0.5vw;  border-radius: 0.3vw 0.3vw 0.3vw 0.3vw;background-color: rgba(0, 0, 0, 0.6);"
  milad.innerHTML = "<span>Milad</span>";
  document.getElementById('player-header').appendChild(milad);
  
  var milad = document.createElement('div');
- milad.className = "designer";
+ milad.className = "designe";
  milad.innerHTML = "<small>Designed By MiLaD#0125</small>";
  milad.style.cssText = "display:none !important;top: -1.0vw;letter-spacing: 0.05vw;position: absolute;padding: 0 0.2vw;display:block !important;transition:0.3s;background: rgba(0,0,0,0.5);font-size: 0.6vw;border-radius: 0.2vw 0.2vw 0.2vw 0.2vw;color: #fff;right: 2vw;"
  document.getElementById('player-header').appendChild(milad);
